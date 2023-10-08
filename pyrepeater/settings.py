421510436176fd9d68303ea73d6cfef6b7eb2fd9
Eric Muehlstein <muehlbucks@gmail.com>
2023-10-07 20:25:49 -0500

    docstrings

--- pyrepeater/settings.py
+++ pyrepeater/settings.py
@@ -1,9 +1,13 @@
+""" settings for pyrepeater"""
+
 from pydantic_settings import BaseSettings
 
 
 class RepeaterSettings(BaseSettings):
+    """settings for pyrepeater"""
+
     fcc_id: str = "WRXC682"
     serial_port: str = "/dev/tty.usbserial-FTDCZX23"
-    pre_tx_delay: float = 1 # seconds between serial tx enable and playing wav file
-    id_mins: int = 15 # minutes between ID messages
-    rpt_info_mins: int = 60 # minutes between repeater info messages
+    pre_tx_delay: float = 1  # seconds between serial tx enable and playing wav file
+    id_mins: int = 15  # minutes between ID messages
+    rpt_info_mins: int = 60  # minutes between repeater info messages
